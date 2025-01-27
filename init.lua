d08ca3d5a35a8c55246c01fb3c61479eed46f354
Uraxii <accounts@brandonpaul.me>
2025-01-27 01:22:56 -0500

    Fixed issues with floating window highlighting; Created script that runs at startup to show neotree if nvim was opened in a directory.

--- init.lua
+++ init.lua
@@ -2,6 +2,9 @@ require 'vim_settings'
 require 'keymaps'
 require 'autocommands'
 require 'lazy_plugin_manager_setup'
+require 'fix_highlights'
+
+require 'show_file_explorer_plugin'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -4,23 +4,42 @@
 
 return {
   'nvim-neo-tree/neo-tree.nvim',
+
   version = '*',
+
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
+
   cmd = 'Neotree',
+
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
+
   opts = {
     close_if_last_window = true,
+    popup_border_style = 'rounded',
     filesystem = {
       window = {
-        position = 'right',
+        position = 'float',
+        popup = {
+          size = { height = '30', width = '90' },
+          position = '50%', -- 50% means center it
+          boder = 'none',
+        },
         mappings = {
           ['\\'] = 'close_window',
+          ['P'] = {
+            'toggle_preview',
+            config = {
+              use_float = true,
+              use_image_nvim = true,
+              title = 'Neo-tree Preview',
+            },
+          },
         },
       },
       filtered_items = {
